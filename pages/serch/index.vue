87f1f9053e26cb2c9eb2849874343e82144e29a5
i19fukuda <i19fukuda1k@gmail.com>
2020-08-19 02:52:46 +0900

    検索ページの一覧に戻るリンクを作成した

---
+++ pages/serch/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="container">
+    <h1 class="tile">イベント検索</h1>
+      <ul>
+        <li><nuxt-link to="/serch/stage">ステージイベントを検索</nuxt-link></li>
+        <li><nuxt-link to="/serch/food">食べる所を検索</nuxt-link></li>
+        <li><nuxt-link to="/serch/omise">体験型のイベントを検索</nuxt-link></li>
+        <li><nuxt-link to="/serch/allEvent">すべての企画を検索</nuxt-link></li>
+      </ul>
+  </section>
+</template>
