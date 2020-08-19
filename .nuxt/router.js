6c75590573de46dd91acd6ccfc57780db69a2dfe
YanagiTakafumi <takafumi0105@i.softbank.jp>
2020-08-19 15:49:54 +0900

    omaru -> omaturi

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,6 +3,11 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
+const _2bd63a0a = () => interopDefault(import('../pages/serch/index.vue' /* webpackChunkName: "pages/serch/index" */))
+const _fc5b483a = () => interopDefault(import('../pages/serch/allEvent/index.vue' /* webpackChunkName: "pages/serch/allEvent/index" */))
+const _65d28808 = () => interopDefault(import('../pages/serch/food/index.vue' /* webpackChunkName: "pages/serch/food/index" */))
+const _7d7f9c89 = () => interopDefault(import('../pages/serch/omise/index.vue' /* webpackChunkName: "pages/serch/omise/index" */))
+const _4191d46a = () => interopDefault(import('../pages/serch/stage/index.vue' /* webpackChunkName: "pages/serch/stage/index" */))
 const _56117ff6 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
@@ -22,6 +27,26 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/serch",
+    component: _2bd63a0a,
+    name: "serch"
+  }, {
+    path: "/serch/allEvent",
+    component: _fc5b483a,
+    name: "serch-allEvent"
+  }, {
+    path: "/serch/food",
+    component: _65d28808,
+    name: "serch-food"
+  }, {
+    path: "/serch/omise",
+    component: _7d7f9c89,
+    name: "serch-omise"
+  }, {
+    path: "/serch/stage",
+    component: _4191d46a,
+    name: "serch-stage"
+  }, {
     path: "/",
     component: _56117ff6,
     name: "index"
