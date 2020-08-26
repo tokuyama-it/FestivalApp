91600ee9c89153dc2f9f0805ec0bfd30ec206c65
YanagiTakafumi <takafumi0105@i.softbank.jp>
2020-08-27 08:43:41 +0900

    背景色変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -58,16 +58,7 @@
 }
 .root {
   background: #4776e6; /* fallback for old browsers */
-  background: -webkit-linear-gradient(
-    to right,
-    #8e54e9,
-    #4776e6
-  ); /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(
-    to right,
-    #8e54e9,
-    #4776e6
-  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: linear-gradient(#373b44, #4286f4);
 }
 header {
   width: 100%;
