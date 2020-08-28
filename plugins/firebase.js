069d2d1cd7dfa600cbffdd857a25332e10a56148
shebang-sh <c19yamamoto@gmail.com>
2020-08-28 17:38:14 +0900

    Herokuのエラー対応とパッケージのバージョンの更新

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,14 +2,14 @@ import firebase from "firebase";
 
 if (!firebase.apps.length) {
 firebase.initializeApp({
-    apiKey:process.env.FB_API_KEY,
-    authDomain:process.env.FB_AUTH_DOMAIN,
-    databaseURL:process.env.FB_DATABASE_URL,
-    projectId:process.env.FB_PROJECTID,
-    storageBucket:process.env.FB_STORAGE_BUCKET,
-    messagingSenderId:process.env.FB_MESSAGING_SENDER_ID,
-    appId:process.env.FB_APP_ID,
-    measurementId:process.env.FB_MEASUREMENT_ID
+    apiKey:String(process.env.FB_API_KEY),
+    authDomain:String(process.env.FB_AUTH_DOMAIN),
+    databaseURL:String(process.env.FB_DATABASE_URL),
+    projectId:String(process.env.FB_PROJECTID),
+    storageBucket:String(process.env.FB_STORAGE_BUCKET),
+    messagingSenderId:String(process.env.FB_MESSAGING_SENDER_ID),
+    appId:String(process.env.FB_APP_ID),
+    measurementId:String(process.env.FB_MEASUREMENT_I)
 })
 }
 
